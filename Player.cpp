6b407cb87604b41b4a8e2ce2a03513436a9cbb2b
darnissan <67154616+darnissan@users.noreply.github.com>
2022-12-25 19:31:47 +0200

    Player methods in cpp file

--- Player.cpp
+++ Player.cpp
@@ -14,3 +14,72 @@ Player::Player(int playerID, int teamId, const permutation_t &spirit, int gamesP
     this->gamesTeamPlayedBefore=0;
     this->gamesFromRootPlayer=0;
 }
+Player::~Player()=default;
+int Player::getPlayerID()
+{
+    return this->playerID;
+}
+int Player::getPlayerAbility()
+{
+    return this->playerAbility;
+}
+int Player::getPlayerTeamID()
+{
+    return this->playerTeamID;
+}
+permutation_t Player::getPlayerSpirit()
+{
+    return this->playerSpirit;
+}
+int Player::getPlayerCards()
+{
+    return this->playerCards;
+}
+void Player::addCards(int newCards)
+{
+    this->playerCards+=newCards;
+}
+void Player::setPlayerTeamID(int newTeamID)
+{
+    this->playerTeamID=newTeamID;
+}
+int Player::getGamesPlayedINIT()
+{
+    return this->gamesPlayedINIT;
+}
+int Player::getGamesTeamPlayedBefore()
+{
+    return this->gamesTeamPlayedBefore;
+}
+int Player::getGamesFromRootPlayer()
+{
+    return this->gamesFromRootPlayer;
+}
+bool Player::getIsGoalKeeper()
+{
+    return this->isGoalKeeper;
+}
+bool Player::getIsTeamActive()
+{
+    return this->isTeamActive;
+}
+bool Player::getIsRootPlayer()
+{
+    return this->isRootPlayer;
+}
+void Player::setIsRootPlayer(bool isRootPlayer)
+{
+    this->isRootPlayer=isRootPlayer;
+}
+void Player::setIsTeamActive(bool isTeamActive)
+{
+    this->isTeamActive=isTeamActive;
+}
+void Player::setGamesTeamPlayedBefore(int gamesTeamPlayedBefore)
+{
+    this->gamesTeamPlayedBefore=gamesTeamPlayedBefore;
+}
+void Player::setGamesFromRootPlayer(int gamesFromRootPlayer)
+{
+    this->gamesFromRootPlayer=gamesFromRootPlayer;
+}

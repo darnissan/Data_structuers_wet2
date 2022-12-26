240547ff837455d30233ea268d860f3accf6f4a1
darnissan <67154616+darnissan@users.noreply.github.com>
2022-12-26 20:22:06 +0200

    class for team and abilitis

---
+++ TeamAndAbilities.cpp
@@ -0,0 +1,37 @@
+#include "TeamAndAbilities.h" 
+
+TeamAndAbilities:: TeamAndAbilities(int teamID, int sumPlayersAbility)
+{
+    this->teamID=teamID;
+    this->sumPlayersAbility=sumPlayersAbility;
+}
+TeamAndAbilities::~TeamAndAbilities()=default;
+int TeamAndAbilities::getTeamID()
+{
+    return this->teamID;
+}
+int TeamAndAbilities::getSumPlayersAbility()
+{
+    return this->sumPlayersAbility;
+}
+bool TeamAndAbilities::operator ==(const TeamAndAbilities& team)
+{
+    return this->teamID==team.teamID;
+}
+bool TeamAndAbilities::operator >(const TeamAndAbilities& team)
+{
+    if (this->sumPlayersAbility==team.sumPlayersAbility)
+    {
+        return this->teamID>team.teamID;
+    }
+    return this->sumPlayersAbility>team.sumPlayersAbility;
+}
+bool TeamAndAbilities::operator <(const TeamAndAbilities& team)
+{
+    if (this->sumPlayersAbility==team.sumPlayersAbility)
+    {
+        return this->teamID<team.teamID;
+    }
+    return this->sumPlayersAbility<team.sumPlayersAbility;
+}
+
---
+++ TeamAndAbilities.h
@@ -0,0 +1,24 @@
+#ifndef TEAM_AND_ABILITIES_H_
+#define TEAM_AND_ABILITIES_H_
+
+#include "wet2util.h"
+#include "AvlTree.h"
+
+class TeamAndAbilities
+{
+    private:
+    int teamID;
+    int sumPlayersAbility;
+   
+
+
+    public:
+    TeamAndAbilities(int teamID, int sumPlayersAbility);
+    ~TeamAndAbilities();
+    int getTeamID();
+    int getSumPlayersAbility();
+    bool operator ==(const TeamAndAbilities& team);
+    bool operator >(const TeamAndAbilities& team);
+    bool operator <(const TeamAndAbilities& team);
+};
+#endif // TEAM_AND_ABILITIES_H_
